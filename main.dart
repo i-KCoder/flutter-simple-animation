f84209a8f03f07cb590d12cffe953d17288d5df5
i-KCoder <119971302+i-KCoder@users.noreply.github.com>
2023-01-06 01:46:44 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'animation_widget.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -43,4 +41,52 @@ class _MyHomePageState extends State<MyHomePage> {
       ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+
+
+class AnimationWidget extends StatefulWidget {
+  const AnimationWidget({super.key});
+
+  @override
+  State<StatefulWidget> createState() => AnimationWidgetState();
+}
+class AnimationWidgetState extends State<AnimationWidget> with TickerProviderStateMixin{
+
+  late AnimationController controller;
+  late Animation colorAnimation;
+  late Animation sizeAnimation;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    colorAnimation = ColorTween(begin: Colors.blue, end: Colors.purple).animate(controller);
+    sizeAnimation = Tween(begin: 100.0, end: 200.0).animate(controller);
+    controller.addListener(() {
+      setState(() {
+
+      });
+    });
+    controller.repeat();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          height: sizeAnimation.value,
+          width: sizeAnimation.value,
+          color: colorAnimation.value
+      ),
+    );
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
 }
